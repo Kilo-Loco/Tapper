8b8f9e1db785ca4cead32e59e0646050a3c0af45
Kyle Lee <KiloLoco@Kyles-MacBook-Pro.local>
2015-10-28 15:58:20 -0700

    Initialized Tapper on Github

--- Tapper/ViewController.swift
+++ Tapper/ViewController.swift
@@ -9,17 +9,83 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    // Properties
+    var maxTaps : Int = 0
+    var currentTaps : Int = 0
+    
+    // Variables
+    @IBOutlet weak var logoImg : UIImageView!
+    @IBOutlet weak var howManyTapsText : UITextField!
+    @IBOutlet weak var playButton : UIButton!
+    
+    @IBOutlet weak var tapButton : UIButton!
+    @IBOutlet weak var tapsLabel : UILabel!
+    @IBOutlet weak var balloon : UIImageView!
+    
+    @IBAction func tapperTapped(sender: UIButton!) {
+        currentTaps++
+        updateTapsLabel()
+        
+        if isGameOver() {
+            restartGame()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // Start game function
+    @IBAction func onPlayButtonPressed(sender: UIButton!) {
+        if let howManyTapsNumber: Int = Int(howManyTapsText.text!)!{
+        
+            if howManyTapsNumber > 0 && howManyTapsText.text != "" {
+        
+                // Hide splash screen controls
+                logoImg.hidden = true
+                howManyTapsText.hidden = true
+                playButton.hidden = true
+            
+                // Show game controls
+                tapButton.hidden = false
+                tapsLabel.hidden = false
+                balloon.hidden = false
+            
+                maxTaps = Int(howManyTapsText.text!)!
+                currentTaps = 0
+            
+                updateTapsLabel()
+            
+                if isGameOver() {
+                    restartGame()
+                }
+            }
+        }
     }
+    
+    func isGameOver() -> Bool {
+        if currentTaps >= maxTaps {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func restartGame() {
+        maxTaps = 0
+        howManyTapsText.text = ""
+        
+        // Hide splash screen controls
+        logoImg.hidden = false
+        howManyTapsText.hidden = false
+        playButton.hidden = false
+        
+        // Show game controls
+        tapButton.hidden = true
+        tapsLabel.hidden = true
+        balloon.hidden = true
 
+    }
+    
+    func updateTapsLabel() {
+        tapsLabel.text = "\(currentTaps) Taps"
+    }
 
 }
 
